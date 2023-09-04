ce5a4f43a94c64e330fcf8ceada0a40f5fb4e853
LinaAlfawzan <442200438@student.ksu.edu.sa>
2023-09-04 14:11:13 +0300

    lab2 lina

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     TextView receiver_msg;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         send_button= findViewById(R.id.send_button_id);
